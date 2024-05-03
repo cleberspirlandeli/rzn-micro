1774089557c0290b873d689a488dd0647f8979d9
Cleber Rezende Spirlandeli <compasso-cleber.spirlandeli@ailos.coop.br>
2024-05-03 17:37:57 -0300

    feat: Development message brocker AWS SQS - #20240503

---
+++ rzn-micro/src/RznMicro.Atlanta.AwsSQS/PublisherQueue.cs
@@ -0,0 +1,38 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+using RznMicro.Atlanta.Core;
+using RznMicro.Atlanta.Core.AppSetting;
+using RznMicro.Atlanta.Core.RequestContext;
+using System.Text.Json;
+
+namespace RznMicro.Atlanta.AwsSQS;
+
+public class PublisherQueue : IPublisherQueue
+{
+    private readonly BasicAWSCredentials _credentials;
+    private readonly AmazonSQSClient _client;
+    private readonly AppSettings _appSettings;
+
+    public PublisherQueue(IOptions<AppSettings> appSettings)
+    {
+        _appSettings = appSettings.Value;
+        _credentials = new BasicAWSCredentials(_appSettings.AWS.Credentials.AccessKey, _appSettings.AWS.Credentials.SecretAccessKey);
+        _client = new AmazonSQSClient(_credentials, RegionEndpoint.USEast1);
+    }
+
+    public async Task<string> PublishAsync<TMessage>(string queueUrl, TMessage messageBody) where TMessage : IMessage
+    {
+        var request = new SendMessageRequest
+        {
+            QueueUrl = queueUrl,
+            MessageBody = JsonSerializer.Serialize(messageBody),
+        };
+
+        var result = await _client.SendMessageAsync(request);
+
+        return result.MessageId;
+    }
+}
