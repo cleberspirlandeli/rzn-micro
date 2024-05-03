36061121d3221eb3896e4c7c9145109be1d8e40c
Cleber Rezende Spirlandeli <compasso-cleber.spirlandeli@ailos.coop.br>
2024-05-03 17:38:10 -0300

    feat: Development message brocker AWS DynamoDB - #20240503

---
+++ rzn-micro/src/RznMicro.Atlanta.Dynamodb/Configurations/DynamoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Microsoft.Extensions.Options;
+using RznMicro.Atlanta.Core.AppSetting;
+
+namespace RznMicro.Atlanta.DynamoDB.Configurations;
+
+public class DynamoDBContext
+{
+    private readonly AppSettings _appSettings;
+    private readonly AmazonDynamoDBClient _amazonDynamoDBClient;
+
+    public DynamoDBContext(IOptions<AppSettings> appSettings)
+    {
+        _appSettings = appSettings.Value;
+        _amazonDynamoDBClient = new AmazonDynamoDBClient(_appSettings.AWS.Credentials.AccessKey, _appSettings.AWS.Credentials.SecretAccessKey, RegionEndpoint.USEast1);
+    }
+}
